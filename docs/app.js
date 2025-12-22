240d3bbbe981e51b2529fe823e1a68c4977e94bc
kesiabomfa <52920939+kesiabomfa@users.noreply.github.com>
2025-12-22 18:35:43 -0300

    Fix URL format for CKAN dataset

--- docs/app.js
+++ docs/app.js
@@ -1,5 +1,5 @@
 const urlCKAN =
-  "https://dados.mg.gov.br/dataset/relacao-nominal-servidores/resource/dc57c390-dda8-4360-8bfb-0474231108cd/download/dados_serv_202501.csv";
+  "https://dados.mg.gov.br/dataset/relacao_nominal_servidores/resource/dc57c390-dda8-4360-8bfb-0474231108cd/download/dados_serv_202501.csv";
 
 const urlComProxy = "https://corsproxy.io/?" + encodeURIComponent(urlCKAN);
 
