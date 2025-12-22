59a43ed953098edd64a9801646e2c66efe25fff9
kesiabomfa <52920939+kesiabomfa@users.noreply.github.com>
2025-12-22 16:50:24 -0300

    Update CSV URL and add proxy for fetch request

--- docs/app.js
+++ docs/app.js
@@ -1,8 +1,10 @@
-const URL_SERVIDORES =
-  "https://dados.mg.gov.br/dataset/relacao_nominal_servidores/resource/UUID/download/dados_serv_202510.csv";
+const urlCKAN =
+  "https://dados.mg.gov.br/dataset/relacao_nominal_servidores/resource/dc57c390-dda8-4360-8bfb-0474231108cd/download/dados_serv_202501.csv";
 
-fetch(URL_SERVIDORES)
+const urlComProxy = "https://corsproxy.io/?" + encodeURIComponent(urlCKAN);
+
+fetch(urlComProxy)
   .then(r => r.text())
   .then(csv => {
-    console.log("CSV carregado do dados.mg");
+    console.log("CSV carregado com sucesso");
   });
